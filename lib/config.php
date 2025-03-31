c47e291144cc4157494d1c98595975751899df6e
jay jariwala <jariwaltrial@gmail.com>
2025-03-31 21:53:50 +0530

    have change api key

--- lib/config.php
+++ lib/config.php
@@ -3,7 +3,7 @@
 define('AI_MODEL', 'deepseek'); // Options: 'gemini' or 'deepseek'
 define('GEMINI_API_KEY', 'AIzaSyDdKHmIzLbGBKIX_j2DWm8Lg4Jqy4CihYo');
 define('GEMINI_API_URL', 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent');
-define('DEEPSEEK_API_KEY', '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');
+define('DEEPSEEK_API_KEY', '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');
 define('DEEPSEEK_API_URL', 'https://openrouter.ai/api/v1/chat/completions');
 
 // File Repository Configuration
